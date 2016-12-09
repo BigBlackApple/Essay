2d5bf20aba17ebdbb671368c467291a49218bd29
sunyunxin <403023758@qq.com>
2016-12-09 15:45:19 +0800

    Initial Commit

---
+++ Essay/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Essay
+//
+//  Created by Frank_s on 16/12/9.
+//  Copyright © 2016年 Frank_s. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
