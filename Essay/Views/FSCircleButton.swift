7e47ccf7123b30c68331b27469135f03843abbbe
sunyunxin <403023758@qq.com>
2016-12-14 17:51:48 +0800

    heh

---
+++ Essay/Views/FSCircleButton.swift
@@ -0,0 +1,21 @@
+//
+//  FSCircleButton.swift
+//  Essay
+//
+//  Created by Frank_s on 16/12/13.
+//  Copyright © 2016年 Frank_s. All rights reserved.
+//
+
+import UIKit
+
+class FSCircleButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
